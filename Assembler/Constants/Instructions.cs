62178976de564c06517db63fff2b3417de69325f
Adam Kinnell <adamkinnell13@gmail.com>
2016-10-23 21:46:07 +1100

    moved instruction definitions to factory

--- Assembler/Constants/Instructions.cs
+++ Assembler/Constants/Instructions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics.CodeAnalysis;
 using Assembler.Instructions;
 using Assembler.Operands;
 using Assembler.Operands.Types;
@@ -33,88 +32,10 @@ namespace Assembler.Constants {
             RegisterMiscInstructions(INSTRUCTION_REGISTRY);
         }
 
-        // Helper Functions ///////////////////////////////////////////////////
+        // Static Functions////////////////////////////////////////////////////
 
         /// <summary>
-        ///     Get a mapping builder for an RR type instruction.
-        /// 
-        ///     R1 = First Operand
-        ///     R2 = Second Operand
-        ///     i.e. MNEM $R1, $R2
-        /// </summary>
-        [NotNull]
-        [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
-        private static InstructionFieldMappingBuilder GetRRBuilder()
-            => new InstructionFieldMappingBuilder()
-                .R1(ops => (int) ((RegisterOperand) ops[0]).RegisterNumber)
-                .R2(ops => (int) ((RegisterOperand) ops[1]).RegisterNumber)
-                .Immediate(0);
-
-        /// <summary>
-        ///     Get a mapping builder for an RI type instruction.
-        /// 
-        ///     R1 = First Operand
-        ///     R2 = Second Operand
-        ///     IMM = Third Operand
-        ///     i.e. MNEM $R1, $R2, IMM
-        /// </summary>
-        [NotNull]
-        [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
-        private static InstructionFieldMappingBuilder GetRIBuilder()
-            => new InstructionFieldMappingBuilder()
-                .R1(ops => (int) ((RegisterOperand) ops[0]).RegisterNumber)
-                .R2(ops => (int) ((RegisterOperand) ops[1]).RegisterNumber)
-                .Immediate(ops => ((ImmediateOperand) ops[2]).Value);
-
-        /// <summary>
-        ///     Get a mapping builder for an RI type instruction
-        ///     where the destination register is unused.
-        /// 
-        ///     R1 = Zero
-        ///     R2 = First Operand
-        ///     IMM = Second Operand
-        ///     i.e. MNEM $R2, IMM
-        /// </summary>
-        [NotNull]
-        [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
-        private static InstructionFieldMappingBuilder GetRIDestinationUnusedBuilder()
-            => new InstructionFieldMappingBuilder()
-                .R1(ops => (int) Registers.RegisterNumber.ZERO)
-                .R2(ops => (int) ((RegisterOperand) ops[0]).RegisterNumber)
-                .Immediate(ops => ((ImmediateOperand) ops[1]).Value);
-
-        /// <summary>
-        ///     Create a native RR format instruction.
-        /// </summary>
-        [NotNull]
-        private static NativeInstruction CreateRRInstruction([NotNull] string mnemonic, Int32 opcode, Int32 function)
-            => new NativeInstruction(
-                mnemonic: mnemonic,
-                operand_format: new OperandFormat(REGISTER_TYPE, REGISTER_TYPE),
-                mapping: GetRRBuilder()
-                    .Opcode(opcode)
-                    .Function(function)
-                    .Build()
-            );
-
-        /// <summary>
-        ///     Create a native RI format instruction.
-        /// </summary>
-        [NotNull]
-        private static NativeInstruction CreateRIInstruction([NotNull] string mnemonic, Int32 opcode, Int32 function)
-            => new NativeInstruction(
-                mnemonic: mnemonic,
-                operand_format: new OperandFormat(REGISTER_TYPE, REGISTER_TYPE, IMMEDIATE_TYPE),
-                mapping: GetRIBuilder()
-                    .Opcode(opcode)
-                    .Function(function)
-                    .Build()
-            );
-
-        // Instruction Building Functions /////////////////////////////////////
-
-        /// <summary>
-        ///     Register all 3 formats of the AND/NAND/OR/NOR/XOR/XNOR/ADD/SUB instructions.
+        ///     Register RR,RI,RI(C) formats of the AND/NAND/OR/NOR/XOR/XNOR/ADD/SUB instructions.
         /// </summary>
         private static void RegisterALUInstructions([NotNull] InstructionRegistry registry) {
 
@@ -123,7 +44,7 @@ namespace Assembler.Constants {
             const Int32 ALU_RIC_OPCODE = 2;
 
             Tuple<string, Int32>[] alu_instructions = {
-                /* Mnemonic, Function */
+                /* (Mnemonic, Function) */
                 new Tuple<string, Int32>("and", 0),
                 new Tuple<string, Int32>("nand", 1),
                 new Tuple<string, Int32>("or", 2),
@@ -134,16 +55,19 @@ namespace Assembler.Constants {
                 new Tuple<string, Int32>("sub", 7),
             };
 
-            // Add RR, RI, and RIC formats for each instruction.
+            // Add RR, RI, and RI(C) formats for each instruction.
             foreach (var pair in alu_instructions) {
-                registry.Register(CreateRRInstruction(pair.Item1, ALU_RR_OPCODE, pair.Item2));
-                registry.Register(CreateRIInstruction(pair.Item1, ALU_RI_OPCODE, pair.Item2));
-                registry.Register(CreateRIInstruction(pair.Item1 + 'c', ALU_RIC_OPCODE, pair.Item2));
+                registry.Register(InstructionFactory.CreateStandardRRInstruction(
+                                      pair.Item1, ALU_RR_OPCODE, pair.Item2));
+                registry.Register(InstructionFactory.CreateStandardRIInstruction(
+                                      pair.Item1, ALU_RI_OPCODE, pair.Item2));
+                registry.Register(InstructionFactory.CreateStandardRIInstruction(
+                                      pair.Item1 + 'c', ALU_RIC_OPCODE, pair.Item2));
             }
         }
 
         /// <summary>
-        ///     Register both formats of the SLL/SRL/SRA/ROL/ROR instructions.
+        ///     Register RR and RI formats of the SLL/SRL/SRA/ROL/ROR instructions.
         /// </summary>
         private static void RegisterShifterInstructions([NotNull] InstructionRegistry registry) {
 
@@ -151,7 +75,7 @@ namespace Assembler.Constants {
             const Int32 SHIFTER_RI_OPCODE = 4;
 
             Tuple<string, Int32>[] shifter_instructions = {
-                /* Mnemonic, Function */
+                /* (Mnemonic, Function) */
                 new Tuple<string, Int32>("sll", 0),
                 new Tuple<string, Int32>("srl", 1),
                 new Tuple<string, Int32>("sra", 2),
@@ -161,14 +85,16 @@ namespace Assembler.Constants {
 
             // Add RR and RI formats for each instruction.
             foreach (var pair in shifter_instructions) {
-                registry.Register(CreateRRInstruction(pair.Item1, SHIFTER_RR_OPCODE, pair.Item2));
-                registry.Register(CreateRIInstruction(pair.Item1, SHIFTER_RI_OPCODE, pair.Item2));
+                registry.Register(InstructionFactory.CreateStandardRRInstruction(
+                                      pair.Item1, SHIFTER_RR_OPCODE, pair.Item2));
+                registry.Register(InstructionFactory.CreateStandardRIInstruction(
+                                      pair.Item1, SHIFTER_RI_OPCODE, pair.Item2));
             }
 
         }
 
         /// <summary>
-        ///     Register both formats of the CEQ/CNEQ/CLT/CLTE/CLTU/CLTEU/CGT/CGTU instructions.
+        ///     Register RR and RI formats of the CEQ/CNEQ/CLT/CLTE/CLTU/CLTEU/CGT/CGTU instructions.
         /// </summary>
         private static void RegisterComparisonInstructions([NotNull] InstructionRegistry registry) {
 
@@ -176,7 +102,7 @@ namespace Assembler.Constants {
             const Int32 COMPARATOR_RI_OPCODE = 6;
 
             Tuple<string, Int32>[] comparison_instructions = {
-                /* Mnemonic, Function */
+                /* (Mnemonic, Function) */
                 new Tuple<string, Int32>("ceq", 0),
                 new Tuple<string, Int32>("cneq", 1),
                 new Tuple<string, Int32>("clt", 2),
@@ -189,8 +115,10 @@ namespace Assembler.Constants {
 
             // Add RR and RI formats for each instruction.
             foreach (var pair in comparison_instructions) {
-                registry.Register(CreateRRInstruction(pair.Item1, COMPARATOR_RR_OPCODE, pair.Item2));
-                registry.Register(CreateRIInstruction(pair.Item1, COMPARATOR_RI_OPCODE, pair.Item2));
+                registry.Register(InstructionFactory.CreateStandardRRInstruction(
+                                      pair.Item1, COMPARATOR_RR_OPCODE, pair.Item2));
+                registry.Register(InstructionFactory.CreateRIDestinationUnusedInstruction(
+                                      pair.Item1, COMPARATOR_RI_OPCODE, pair.Item2));
             }
         }
 
---
+++ Assembler/Instructions/InstructionFactory.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Assembler.Constants;
+using Assembler.Operands;
+using Assembler.Operands.Types;
+using JetBrains.Annotations;
+
+namespace Assembler.Instructions {
+
+    /// <summary>
+    ///     Contains methods for creating instruction definitions.
+    /// </summary>
+    class InstructionFactory {
+
+        // Static Fields //////////////////////////////////////////////////////
+
+        private static readonly Type REGISTER_TYPE = typeof(RegisterOperand);
+        private static readonly Type IMMEDIATE_TYPE = typeof(ImmediateOperand);
+        private static readonly Type BASEOFFSET_TYPE = typeof(BaseOffsetOperand);
+
+        // Static Functions ///////////////////////////////////////////////////
+
+        /// <summary>
+        ///     Create a standard RR format instruction.
+        /// 
+        ///     With Mapping:
+        ///     -   R1  = First Operand
+        ///     -   R2  = Second Operand
+        ///     -   IMM = 0
+        ///     i.e. MNEM $R1, $R2
+        /// </summary>
+        [NotNull]
+        public static NativeInstruction CreateStandardRRInstruction([NotNull] string mnemonic, Int32 opcode, Int32 function)
+            => new NativeInstruction(
+                mnemonic: mnemonic,
+                operand_format: new OperandFormat(REGISTER_TYPE, REGISTER_TYPE),
+                mapping: new InstructionFieldMappingBuilder()
+                    .R1(ops => (int) ((RegisterOperand) ops[0]).RegisterNumber)
+                    .R2(ops => (int) ((RegisterOperand) ops[1]).RegisterNumber)
+                    .Immediate(0)
+                    .Opcode(opcode)
+                    .Function(function)
+                    .Build()
+            );
+
+        /// <summary>
+        ///     Create a standard RI format instruction.
+        /// 
+        ///     With Mapping:
+        ///     -   R1 = First Operand
+        ///     -   R2 = Second Operand
+        ///     -   IMM = Third Operand
+        ///     i.e. MNEM $R1, $R2, IMM
+        /// </summary>
+        [NotNull]
+        public static NativeInstruction CreateStandardRIInstruction([NotNull] string mnemonic, Int32 opcode, Int32 function)
+            => new NativeInstruction(
+                mnemonic: mnemonic,
+                operand_format: new OperandFormat(REGISTER_TYPE, REGISTER_TYPE, IMMEDIATE_TYPE),
+                mapping: new InstructionFieldMappingBuilder()
+                    .R1(ops => (int) ((RegisterOperand) ops[0]).RegisterNumber)
+                    .R2(ops => (int) ((RegisterOperand) ops[1]).RegisterNumber)
+                    .Immediate(ops => ((ImmediateOperand) ops[2]).Value)
+                    .Opcode(opcode)
+                    .Function(function)
+                    .Build()
+            );
+
+        /// <summary>
+        ///     Create an RI format instruction
+        ///     where the destination register is unused.
+        /// 
+        ///     With Mapping:
+        ///     -   R1 = Zero
+        ///     -   R2 = First Operand
+        ///     -   IMM = Second Operand
+        ///     i.e. MNEM $R2, IMM
+        /// </summary>
+        [NotNull]
+        public static NativeInstruction CreateRIDestinationUnusedInstruction([NotNull] string mnemonic, Int32 opcode, Int32 function)
+            => new NativeInstruction(
+                mnemonic: mnemonic,
+                operand_format: new OperandFormat(REGISTER_TYPE, IMMEDIATE_TYPE),
+                mapping: new InstructionFieldMappingBuilder()
+                    .R1(ops => (int) Registers.RegisterNumber.ZERO)
+                    .R2(ops => (int) ((RegisterOperand) ops[0]).RegisterNumber)
+                    .Immediate(ops => ((ImmediateOperand) ops[1]).Value)
+                    .Opcode(opcode)
+                    .Function(function)
+                    .Build()
+            );
+
+    }
+}
